b2cd0b0e09a09041cbbef8322ff46c1c0f9050b6
vishnugowtham97 <vishnugowtham97@gmail.com>
2023-05-20 15:17:01 +0530

    add commit

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://stackoverflowapi-bz4j.onrender.com",
+});
 
 // API.interceptors.request.use((req) => {
 //   if (localStorage.getItem("Profile")) {
